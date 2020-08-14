738dcdc4d4025d9c938e628766982b24924d13ca
Seno <senoadiw@gmail.com>
2020-08-14 22:16:04 +0700

    update: add ckan.onedataportal.geoportal_url and ckan.onedataportal.geoportal_name config settings

--- ckanext/onedataportal/plugin.py
+++ ckanext/onedataportal/plugin.py
@@ -31,6 +31,25 @@ def naive_to_utc(dt, is_dst=None):
     local_dt = local.localize(dt, is_dst=is_dst)
     return local_dt.astimezone(pytz.utc)
 
+def geoportal_url():
+    '''Return string value of the geoportal url, the value must be a valid url
+    '''
+    from ckan.common import config
+    value = config.get('ckan.onedataportal.geoportal_url', False)
+    if value:
+        if value.lower().startswith(('http://', 'https://', '/')):
+            return value
+        else:
+            return False
+    return value
+
+def geoportal_name():
+    '''Return string value of the geoportal name, if empty return default
+    '''
+    from ckan.common import config
+    value = config.get('ckan.onedataportal.geoportal_name', 'Geoportal')
+    return value
+
 
 class OnedataportalPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -47,4 +66,9 @@ class OnedataportalPlugin(plugins.SingletonPlugin):
         # Template helper function names should begin with the name of the
         # extension they belong to, to avoid clashing with functions from
         # other extensions.
-        return {'onedataportal_add_time': add_time, 'onedataportal_naive_to_utc': naive_to_utc}
+        return {
+            'onedataportal_add_time': add_time,
+            'onedataportal_naive_to_utc': naive_to_utc,
+            'onedataportal_geoportal_url': geoportal_url,
+            'onedataportal_geoportal_name': geoportal_name
+        }
